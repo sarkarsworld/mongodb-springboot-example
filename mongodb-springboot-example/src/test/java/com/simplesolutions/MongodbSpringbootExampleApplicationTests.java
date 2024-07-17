c6d28d4d793d71bfe2db731d928f3b2a05ba476c
aloks <sarkarsworld@gmail.com>
2024-07-17 19:53:11 +0530

    initial project structure

---
+++ mongodb-springboot-example/src/test/java/com/simplesolutions/MongodbSpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplesolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbSpringbootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
